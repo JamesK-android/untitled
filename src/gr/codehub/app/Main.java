b3730f5dd6736a79a6275c488257a8f95ee451eb
James <dkatopis@athtech.gr>
2020-04-29 22:31:01 +0300

    29-4-20

---
+++ src/gr/codehub/app/Main.java
@@ -0,0 +1,11 @@
+package gr.codehub.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ui ui = new Ui();
+        Basket basket = new Basket();
+        ui.manageBasket(basket);
+    }
+
+}
